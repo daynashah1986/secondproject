4a367cc3d1705521f90e1ec6a080232b1a04d55e
Kamlesh Shah <Kamlesh Shah@LAPTOP-JL6INC90>
2023-03-01 17:23:52 -0600

    I am uploading all files to github

---
+++ src/Practice/welcome.java
@@ -0,0 +1,12 @@
+package Practice;
+
+public class welcome {
+
+	public static void main(String[] args) {
+		// This is my second project for testing CI concept
+		
+		System.out.println("Welcome to the codelab training session");
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Kamlesh Shah
+ *
+ */
+module SecondProject {
+}
